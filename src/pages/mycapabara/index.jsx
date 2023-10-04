81653574a08867b9b6ac0382e76378160dfe7709
Sam Williams <sam@straitsinteractive.com>
2023-10-04 10:40:35 +0800

    modified alt tags

--- src/pages/mycapabara/index.jsx
+++ src/pages/mycapabara/index.jsx
@@ -27,21 +27,21 @@ export default function MyCapabara() {
               <Badge.Ribbon text="Develop" color="#0C4A6E">
                 <a href="https://aitoolkit.capabara.com">
                   <Card className={styles.appCard}>
-                    <img className={styles.appImage} src="./CapabaraR-AIAssistantPrimary.png" alt="image"/>
+                    <img className={styles.appImage} src="./CapabaraR-AIAssistantPrimary.png" alt="Capability develop" />
                   </Card>
                 </a>
               </Badge.Ribbon>
               <Badge.Ribbon text="Govern" color="#0C4A6E">
                 <a href="https://govern.capabara.com">
                   <Card className={styles.appCard}>
-                    <img className={styles.appImage} src="./CapabaraR-KnowledgeSystemPrimary.png" alt="image"/>
+                    <img className={styles.appImage} src="./CapabaraR-KnowledgeSystemPrimary.png" alt="Capability govern" />
                   </Card>
                 </a>
               </Badge.Ribbon>
               <Badge.Ribbon text="Manage" color="#0C4A6E">
                 <a href="https://manage.capabara.com">
                   <Card className={styles.appCard}>
-                    <img className={styles.appImage} src="./CapabaraR-CapabilityManagementPrimary.png" alt="image"/>
+                    <img className={styles.appImage} src="./CapabaraR-CapabilityManagementPrimary.png" alt="Capability manage" />
                   </Card>
                 </a>
               </Badge.Ribbon>
@@ -51,7 +51,7 @@ export default function MyCapabara() {
           <div className={styles.advertContainer}>
             <Row type="flex">
               <Card className={styles.advertCard}>
-                <img className={styles.advertImage} src="./platform_test_animation_3b.gif" alt="image"/>
+                <img className={styles.advertImage} src="./platform_test_animation_3b.gif" alt="rolling advert" />
               </Card>
             </Row>
           </div>
@@ -62,37 +62,37 @@ export default function MyCapabara() {
             <Row type="flex" className={styles.courseRow}>
 
               <a href="https://dpoinbox.genexist.com/">
-                <Card cover={<img src="./dpoinbox.png" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./dpoinbox.png" alt="DPOinBox Logo" />} className={styles.courseCard}>
                   DPOinBox
                 </Card>
               </a>
 
               <a href="https://www.dpexnetwork.org/courses">
-                <Card cover={<img src="./6264aa943c33126392a9aeb2331077e5.webp" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./6264aa943c33126392a9aeb2331077e5.webp" alt="Course details" />} className={styles.courseCard}>
                   Certified AI Business Professional
                 </Card>
               </a>
 
               <a href="https://www.dpexnetwork.org/courses">
-                <Card cover={<img src="./d3be769196df5b139926bf0f69448b27.webp" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./d3be769196df5b139926bf0f69448b27.webp" alt="Course details" />} className={styles.courseCard}>
                   Practical Approach to Generative AI
                 </Card>
               </a>
 
               <a href="https://www.dpexnetwork.org/courses">
-                <Card cover={<img src="./Dx8vGRM4Eco9kLwYwBE2cA.png" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./Dx8vGRM4Eco9kLwYwBE2cA.png" alt="Course details" />} className={styles.courseCard}>
                   Conversational AI and Prompt Techniques
                 </Card>
               </a>
 
               <a href="https://www.dpexnetwork.org/courses">
-                <Card cover={<img src="./cu5DFACrgq3rJkDxHVJ6jt.png" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./cu5DFACrgq3rJkDxHVJ6jt.png" alt="Course details" />} className={styles.courseCard}>
                   Applications of Generative AI and Business Productivity
                 </Card>
               </a>
 
               <a href="https://www.dpexnetwork.org/courses">
-                <Card cover={<img src="./7PTKskh9b67KAXFC7eUqmG.png" alt="image"/>} className={styles.courseCard}>
+                <Card cover={<img src="./7PTKskh9b67KAXFC7eUqmG.png" alt="Course details" />} className={styles.courseCard}>
                   Data Ethics and AI Governance Frameworks
                 </Card>
               </a>

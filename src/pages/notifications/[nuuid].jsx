5db6e3f24d773c39452f15f5796624691622fefc
Andrew Fam <andrewfam@Andrews-MacBook-Pro.local>
2023-10-20 18:20:40 +0800

    Fix to notifications

--- src/pages/notifications/[nuuid].jsx
+++ src/pages/notifications/[nuuid].jsx
@@ -1,32 +1,14 @@
 import { useRouter } from 'next/router';
-import qs from 'qs';
-import { useContext } from 'react';
-import useSWR from 'swr';
-import { apiGet } from 'services/api';
-import { userContext } from 'contexts/Auth';
+import { useEffect } from 'react';
 import MainLayout from 'components/layouts/Layout';
 
 const NotifRedirect = () => {
   const router = useRouter();
   const { nuuid } = router.query;
-  const { authToken } = useContext(userContext);
-
-  const onSuccess = (notif) => {
-    let url = '';
-    if (notif.url === 'all-capabilities') {
-      url = `/capabilities?${qs.stringify(notif.url_kwargs)}`;
-    } else {
-      const { obj, get } = notif.url_kwargs;
-      url = `/capabilities/${obj}?${qs.stringify(get)}`;
-    }
-
-    router.push(url);
-  };
-
-  useSWR(
-    router.isReady ? ['notif', nuuid] : null,
-    () => apiGet(`/notifications/${nuuid}/`, authToken, onSuccess)
-  );
+  
+  useEffect(() => {
+    window.location.replace(`https://manage.capabara.com/notifications/${nuuid}`);
+  },[]);
 
   return (
     <MainLayout>
